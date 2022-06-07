02c8764aefee878cb2aab5c9c07076009d9ea9b2
Jack <55797559+jackgroom@users.noreply.github.com>
2022-06-07 02:04:42 +0100

    Update BookServiceTest.java

--- src/test/java/com/jackgroom/book/BookServiceTest.java
+++ src/test/java/com/jackgroom/book/BookServiceTest.java
@@ -20,7 +20,7 @@ class BookServiceTest {
         BookService.addNewBook("TestBook The Sequel", "Jack Groom", BookGenre.MYSTERY, LocalDate.now(), BigDecimal.valueOf(100));
         BookService.addNewBook("Harry Potter", "J.K.Rowling", BookGenre.FANTASY, LocalDate.of(1995, 6, 14), BigDecimal.valueOf(9.99));
         BookService.addNewBook("Lord of the Rings", "Tolkien", BookGenre.FANTASY, LocalDate.of(1934, 10, 29), BigDecimal.valueOf(15));
-        BookService.addNewBook("Fifty Shades of Grey", "Some Loser", BookGenre.ROMANCE, LocalDate.of(2010, 12, 16), BigDecimal.valueOf(12.99));
+        BookService.addNewBook("Fifty Shades of Grey", "Some dude", BookGenre.ROMANCE, LocalDate.of(2010, 12, 16), BigDecimal.valueOf(12.99));
         BookService.addNewBook("Corpse Party", "Some Japanese Guy", BookGenre.HORROR, LocalDate.of(2014, 10, 2), BigDecimal.valueOf(11.37));
         BookService.addNewBook("Star Wars", "George Lucas", BookGenre.SCIFI, LocalDate.of(1960, 4, 4), BigDecimal.valueOf(0.3));
         BookService.addNewBook("Skullduggery Pleasant", "Cant remember", BookGenre.MYSTERY, LocalDate.of(2014, 6, 11), BigDecimal.valueOf(15.37));
